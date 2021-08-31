1617b48de55c1369d6bb9aa2e118124d33bb6f79
Artem <shlemafona@gmail.com>
2021-09-01 01:12:51 +0300

    Revert "Finished (#1)" (#3)
    This reverts commit dac48204897c2a54ccef45e91c6cff97e41de55d.

--- src/app.jsx
+++ src/app.jsx
@@ -1,61 +1,31 @@
 import React, {useState} from 'react';
 // import Users from './components/users';
 import api from './API/index';
-import Pagination from './components/pagination';
 import SearchStatus from './components/searchStatus';
 import UsersTable from './components/usersTable';
 
-function usersToState(users, activePageNum=1) {
-    const USERS_PER_PAGE = 5;
-    const pagesCount = Math.ceil(users.length / USERS_PER_PAGE);
-    const currentPageNum = Math.min(activePageNum, pagesCount);
-
-    const sliceStart = USERS_PER_PAGE * (currentPageNum-1);
-    const sliceEnd = sliceStart + USERS_PER_PAGE;
-    const currentPageUsers = users.slice(sliceStart, sliceEnd);
-
-    return {
-        pagesCount,
-        currentPageNum,
-        currentPageUsers,
-        users
-    }
-}
-
 const App = () => {
     const usersInitialState = api.users.fetchAll();
-    const [paginationState, setPaginationState] = useState(usersToState(usersInitialState));
+    const [users, setUsers] = useState(usersInitialState);
 
-    const handlerUerDelete = (userId) => {
-        const newUsers = paginationState.users.filter(usr => usr._id !== userId);
-        const newState = usersToState(newUsers, paginationState.currentPageNum);
-        setPaginationState(newState);
+    const deleteUserHandler = (userId) => {
+        setUsers(users.filter(usr => usr._id !== userId));
     }
 
     const handlerUserBookmarkToggle = (userId) => {
-        const newUsers = [...paginationState.users];
-        const el = newUsers.find(el => el._id === userId);
-        el.isFavorite = !el.isFavorite; 
-        const newState = usersToState(newUsers, paginationState.currentPageNum);
-        setPaginationState(newState);
-    }
-
-    const handlerPaginationPageSelect = (pageId) => {
-        const newState = usersToState(paginationState.users, pageId);
-        setPaginationState(newState);
+        const newUsersState = [...users];
+        const el = newUsersState.find(el => el._id === userId);
+        el.isFavorite = !el.isFavorite;
+        setUsers(newUsersState);
     }
 
     return ( 
         <>
-            <SearchStatus usersQty={paginationState.users.length}/> 
+            <SearchStatus usersQty={users.length}/> 
             <UsersTable 
-                users={paginationState.currentPageUsers} 
-                onUserDelete={handlerUerDelete}
+                users={users} 
+                onUserDelete={deleteUserHandler}
                 onUserBookmarkClick={handlerUserBookmarkToggle}
-            />
-            <Pagination
-                pageCount={paginationState.pagesCount}
-                onPageSelect={handlerPaginationPageSelect}
             />
         </>
     );
