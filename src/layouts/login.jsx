c27613f916eefea4c16d45f047e7ec80faafbea6
ay <shlemafona@gmail.com>
2021-10-07 17:44:38 +0300

    q3 Finished

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 
 const Login = () => {
-    return (<h1>Login</h1>);
+    const initialState = {email: '', password: ''};
+    const [data, setData] = useState(initialState);
+
+    const handleChange = (e) => {
+        const {name: propName, value} = e.target;
+        setData((prevState) => {
+            return {...prevState, [propName]: value};
+        });
+    };
+
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Пароль</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
